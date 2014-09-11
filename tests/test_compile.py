a43c813122d02d1937ade94dca4ec7134cb9902e
Сосна Евгений <shenja@sosna.zp.ua>
2014-09-11 12:39:31 +0300

    Добавленны тесты.

--- tests/test_compile.py
+++ tests/test_compile.py
@@ -2,21 +2,35 @@ import unittest
 import pyv8unpack
 from os import path as path
 import tempfile
+import shutil
 
 
 class TestV8Unpack(unittest.TestCase):
+    
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+        self.tpath = tempfile.mkdtemp()
+        self.tfile = tempfile.mktemp()
+        
+    def tearDown(self):
+        import os
+        if os.path.exists(self.tfile):
+            os.remove(self.tfile)
+        shutil.rmtree(self.tpath)
+
 
     def test_compile_from_source(self):
-
-        tpath = tempfile.mkdtemp()
+        
+        self.tpath = tempfile.mkdtemp()
         file = path.join(path.curdir, "tests", "Fixture.epf")
-        assert pyv8unpack.decompile([file], tpath)
-        tpath = path.join(tpath, "tests", "Fixture")
-        filenew = tempfile.mktemp()
-        assert pyv8unpack.compilefromsource(tpath, filenew, "epf") == filenew
+        assert pyv8unpack.decompile([file], self.tpath)
+        tpath = path.join(self.tpath, "tests", "Fixture")
+        assert pyv8unpack.compilefromsource(tpath, self.tfile, "epf") == self.tfile
+        self.assertTrue(path.exists(self.tfile), "Собранный файл не существует {}".format(self.tfile))
+        
 
     def test_decompile(self):
-
-        tpath = tempfile.mkdtemp()
+        
+        self.tpath = tempfile.mkdtemp()
         file = path.join(path.curdir, "tests", "Fixture.epf")
-        assert pyv8unpack.decompile([file], tpath)
+        assert pyv8unpack.decompile([file], self.tpath)
