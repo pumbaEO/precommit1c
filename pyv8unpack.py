a086276a8c8c8d71165b1896b504b2c7d1e63d75
Артур Аюханов <aartbear@gmail.com>
2015-11-16 13:49:39 +0300

    Положил утилиту v8unpack как бинарник в каталог tools #46

--- pyv8unpack.py
+++ pyv8unpack.py
@@ -317,9 +317,10 @@ def compilefromsource(input_, output, ext):
 
         # Вызовем v8unpack для сборки файла из исходников
         temp_file = tempfile.mktemp('.' + extfile)
-        unpackpath = findexecute("v8unpack")
-        if len(unpackpath) == 0:
-            unpackpath = findexecute("unpackv8")
+        #unpackpath = findexecute("v8unpack")
+        #if len(unpackpath) == 0:
+        #    unpackpath = findexecute("unpackv8")
+		unpackpath = get_service_path(os.path.join('tools', 'v8unpack.exe'))
         assert (len(unpackpath) > 0), "path to v8unpack is empty"
         log.debug('{} -B "{}" "{}"'.format(unpackpath, '{}'.format(temp_path), temp_file))
         print('{} -B "{}" "{}"'.format(unpackpath, '{}'.format(temp_path), temp_file))
