921f878427da92cb7dfb6d9b431e9a9d3a1d48ca
Артур Аюханов <aartbear@gmail.com>
2015-11-27 15:27:00 +0300

    Копирую утилиту v8unpack как бинарник в каталог tools #46

--- copy-to-hook.cmd
+++ copy-to-hook.cmd
@@ -7,7 +7,8 @@ xcopy .\pre-commit .\..\..\.git\hooks\ /Y /F
 mkdir .\..\..\.git\hooks\v8Reader
 xcopy .\v8Reader\V8Reader.epf .\..\..\.git\hooks\v8Reader\ /Y /F
 xcopy .\pyv8unpack.py .\..\..\.git\hooks\ /Y /F
-xcopy .\tools\v8unpack.exe .\..\..\.git\hooks\ /Y /F
+mkdir .\..\..\.git\hooks\tools
+xcopy .\tools\v8unpack.exe .\..\..\.git\hooks\tools\ /Y /F
 
 cd .\..\..\
 git config --local core.quotepath false
